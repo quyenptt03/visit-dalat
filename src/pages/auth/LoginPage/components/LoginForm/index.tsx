28defd2a5a5042359997613d933b37c3e15db10a
quyenptt03 <quyenptt03@gmail.com>
2023-07-16 21:52:15 +0700

    login page ui

--- src/pages/auth/LoginPage/components/LoginForm/index.tsx
+++ src/pages/auth/LoginPage/components/LoginForm/index.tsx
@@ -17,50 +17,40 @@ import {
 
 type FormData = {
   email: string;
-  name: string;
   password: string;
-  confirmPassword: string;
 };
-const RegisterForm = (onSubmit: any) => {
+const LoginForm = (onSubmit: any) => {
   const schema = yup
     .object({
       email: yup
         .string()
         .required("Please enter you email")
         .email("Please enter a valid email address."),
-      name: yup.string().required("Please enter your name"),
       password: yup
         .string()
         .required("Please enter your password")
-        // .min(6, "must contain 6 characters"),
         .matches(
           /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
           "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
         ),
-      confirmPassword: yup
-        .string()
-        .required("Please confirm your password")
-        .oneOf([yup.ref("password")], "Password not match"),
     })
     .required();
 
   const form = useForm<FormData>({
     defaultValues: {
       email: "",
-      name: "",
       password: "",
-      confirmPassword: "",
     },
     resolver: yupResolver(schema),
   });
   const handleSubmit = (values: any) => {
-    console.log("RegisterForm submit: ", values);
+    console.log("LoginForm submit: ", values);
   };
   return (
     <Container>
       <Greeting>Welcome!</Greeting>
       <Heading>
-        <Title>Sign up to</Title>
+        <Title>Sign in to</Title>
         <SubTitle>Lorem Ipsum is simply</SubTitle>
       </Heading>
       <Form onSubmit={form.handleSubmit(handleSubmit)}>
@@ -70,32 +60,19 @@ const RegisterForm = (onSubmit: any) => {
           placeholder="Enter you email"
           form={form}
         />
-        <InputField
-          name="name"
-          label="name"
-          placeholder="Enter you name"
-          form={form}
-        />
         <PasswordField
           name="password"
           label="password"
           placeholder="Enter you password"
           form={form}
         />
-
-        <PasswordField
-          name="confirmPassword"
-          label="confirm password"
-          placeholder="Confirm your password"
-          form={form}
-        />
-        <SubmitButton type="submit">Register</SubmitButton>
+        <SubmitButton type="submit">Login</SubmitButton>
       </Form>
       <Span>
-        Already have an Account ? <AuthLink href="/login">Login</AuthLink>
+        Don't have an Account ? <AuthLink href="/register">Register</AuthLink>
       </Span>
     </Container>
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
--- src/pages/auth/LoginPage/index.tsx
+++ src/pages/auth/LoginPage/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import LoginForm from "./components/LoginForm";
+import Logo from "../../../components/Logo";
+import LoginBG from "../../../assets/images/login-bg.png";
+
+const LogoContainer = styled.div`
+  ${tw`m-0 laptop:mt-5 laptop:ml-20`}
+`;
+const Container = styled.div`
+  ${tw`flex items-center justify-center`}
+`;
+const Background = styled.img`
+  ${tw`ml-[7%] hidden laptop:block`}
+`;
 
 const LoginPage = () => {
-  return <div></div>;
+  const handleFormSubmit = (values: any) => {
+    console.log("Form submit: ", values);
+  };
+  return (
+    <>
+      <LogoContainer>
+        <Logo theme="dark" />
+      </LogoContainer>
+      <Container>
+        <LoginForm onSubmit={handleFormSubmit} />
+        <Background src={LoginBG} alt="Register background" />
+      </Container>
+    </>
+  );
 };
 
 export default LoginPage;
